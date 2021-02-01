7765ffa13cb33b20ce96f5f05311c78bc4d654a5
erikaosgue <1476@holbertonschool.com>
2021-02-01 01:38:35 -0500

    Update error case for min value of 1 housrglass

--- 13-hourglass_2d_array.py
+++ 13-hourglass_2d_array.py
@@ -2,17 +2,16 @@
 
 def hourglassSum(arr):
     
-    max_value = 0
+    max_value = -9 * 7
     value = 0
     for i in range(0, 4):
         for j in range(0, 4):
             value += arr[i][j] + arr[i][j+1] + arr[i][j+2]
-            # print("1", value)
             value += arr[i+1][j+1]
-            # print("2", value)
             value += arr[i+2][j] + arr[i+2][j+1] + arr[i+2][j+2]
-            # print("3", value)
-            if max_value < value:
+            print("3", value)
+
+            if max_value <= value:
                 max_value = value
             value = 0
     return(max_value)
@@ -28,11 +27,21 @@ if __name__ == '__main__':
     #     ]
 
     arr = [
-            [-9, -9, -9,  1, 1, 1], 
-            [0, -9,  0,  4, 3, 2],
-            [-9, -9, -9,  1, 2, 3],
-            [0,  0,  8,  6, 6, 0],
-            [0,  0,  0, -2, 0, 0],
-            [0,  0,  1,  2, 4, 0]
-    ]
+            [1, 1, 1, 0, 0, 0],
+            [0, 1, 0, 0, 0, 0],
+            [1, 1, 1, 0, 0, 0],
+            [0, 0, 2, 4, 4, 0],
+            [0, 0, 0, 2, 0, 0],
+            [0, 0, 1, 2, 4, 0]
+        ]
+
+
+    # arr = [
+    #         [-9, -9, -9,  1, 1, 1], 
+    #         [0, -9,  0,  4, 3, 2],
+    #         [-9, -9, -9,  1, 2, 3],
+    #         [0,  0,  8,  6, 6, 0],
+    #         [0,  0,  0, -2, 0, 0],
+    #         [0,  0,  1,  2, 4, 0]
+    # ]
     print(hourglassSum(arr))
