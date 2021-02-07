e5018950b8ffb1be08f0fc1e4c93694d37dfca19
erikaosgue <1476@holbertonschool.com>
2021-02-06 23:51:35 -0500

    First version of minimun_swaps_2

---
+++ medium_challenges/1-minimun_swaps_2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the minimumSwaps function below.
+def minimumSwaps(arr):
+    
+
+if __name__ == '__main__':
+
+    arr = [7,1,3,2,4,5,6]
+    arr = [4,3,1,2]
+    arr = [2,3,4,1,5] # 3
+    arr = [1,3,5,2,4,6,7] # 3
+
+    minimumSwaps(arr)
